435ba7d7e989292a7a87d7c985f816e04c7b77eb
arturajupov <37012636+arturajupov@users.noreply.github.com>
2022-04-14 09:35:43 +0600

    Initial commit

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 number = np.random.randint(1, 101) # загадываем число
 count = 0
 
--- game_v2.py
+++ game_v2.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 def random_predict(number:int=1) -> int:
     """Рандомно угадываем число
 
